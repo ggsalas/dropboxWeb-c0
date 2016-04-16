5b1107ef33371f748b8413457f39e1f585281813
Gabriel Salas <ggsalas@gmail.com>
2016-04-16 13:23:43 -0300

    primitive navigation woring, not work save file

--- src/js/dropboxWeb.js
+++ src/js/dropboxWeb.js
@@ -26,24 +26,59 @@ $.ajax({
         var name = data.entries[i].name;
         var id = data.entries[i].id;
         var path = data.entries[i].path_lower;
+        var tag = data.entries[i][".tag"]; // file or folder
 
         folder.append('<li class="item item-' + i + '">' + name + '</li>');
         $('.item-' + i).click(function(){
           console.log('ok ' + i + ' ' + id);
 
-          // TODO: if is folder, open...
-
-          $.ajax({
-            type: 'post',
-            url: 'https://content.dropboxapi.com/2/files/download',
-            headers: {
-              "Authorization":"Bearer " + ACCESS_TOKEN,
-              "Dropbox-API-Arg":{
-                "path": path,
+          // IF IS FILE 
+          if (tag == "file"){
+            console.log("file");
+            $.ajax({
+              type: 'post',
+              url: 'https://content.dropboxapi.com/2/files/download',
+              headers: {
+                "Authorization": "Bearer JavJ9PXXF9AAAAAAAAAAPoqOU7LbNmvriwVWJLuuJanA6Oh6uVLgfXNfm2PPUfv9",
+                "Dropbox-API-Arg":{
+                  "path": path,
+                },
+                success: (data) => {
+                  window.location = data; // The file not open or save!!
+                }
               },
-            },
-         
-          });
+            });
+          
+          // IF IS FOLDER
+          } else if (tag == "folder"){
+            console.log("folder");
+            $.ajax({
+              type: 'post',
+              data: JSON.stringify({
+                "path": path,
+              }),
+              url: 'https://api.dropboxapi.com/2/files/list_folder',
+              headers: {
+                "Authorization":"Bearer " + ACCESS_TOKEN,
+                "Content-Type":"application/json"
+              },
+              success: (data) => {
+                folder.html('');
+                for(var i = 0; i < data.entries.length; i++){
+                    (function(i){
+                      var name = data.entries[i].name;
+
+                      folder.append('<li class="item item-' + i + '">' + name + '</li>');
+                      $('.item-' + i).click(function(){
+                        console.log('ok ' + i + ' ' + id);
+                      });
+                    })(i)
+                }
+              }
+            });
+          } else {
+            console.log('error ' + tag + name);
+          }
         });
       })(i)
 
