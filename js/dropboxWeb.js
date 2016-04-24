0273c6f5bb3511bf2ba095f9cdf1306e88e95f6c
Carlos Villuendas <carlosvillu@gmail.com>
2016-04-24 17:12:30 +0200

    Updates

--- js/dropboxWeb.js
+++ js/dropboxWeb.js
@@ -1,4 +1,5 @@
 const ACCESS_TOKEN = 'JavJ9PXXF9AAAAAAAAAANxVl8bfZOzTXUuxo3eyXoVKY14lXMl63HS0dpPVDyLzC'
+
 const DATA = {
   "path": "/IntroB_",
   "recursive": false,
@@ -19,14 +20,74 @@ $.ajax({
   success: (data) => {
     folder.append('<ol>');
     for(var i = 0; i < data.entries.length; i++){
-      var name = data.entries[i].name;
 
-      folder.append('<li class="item item-' + i + '">' + name + '</li>');
-      $('.item-' + i).click(function(){
-        console.log('ok ' + i);
-      });
+
+      (function(i){
+        var name = data.entries[i].name;
+        var id = data.entries[i].id;
+        var path = data.entries[i].path_lower;
+        var tag = data.entries[i][".tag"]; // file or folder
+
+        folder.append('<li class="item item-' + i + '">' + name + '</li>');
+        $('.item-' + i).click(function(){
+          console.log('ok ' + i + ' ' + id);
+
+          // IF IS FILE 
+          if (tag == "file"){
+            console.log("file");
+            $.ajax({
+              type: 'post',
+              url: 'https://content.dropboxapi.com/2/files/download',
+              headers: {
+                "Authorization": "Bearer JavJ9PXXF9AAAAAAAAAAPoqOU7LbNmvriwVWJLuuJanA6Oh6uVLgfXNfm2PPUfv9",
+                "Dropbox-API-Arg":{
+                  "path": path,
+                },
+                success: (data) => {
+                  window.location = data; // The file not open or save!!
+                }
+              },
+            });
+          
+          // IF IS FOLDER
+          } else if (tag == "folder"){
+            console.log("folder");
+            $.ajax({
+              type: 'post',
+              data: JSON.stringify({
+                "path": path,
+              }),
+              url: 'https://api.dropboxapi.com/2/files/list_folder',
+              headers: {
+                "Authorization":"Bearer " + ACCESS_TOKEN,
+                "Content-Type":"application/json"
+              },
+              success: (data) => {
+                folder.html('');
+                folder.append('<li class="item">< Volver</li>');
+
+                for(var i = 0; i < data.entries.length; i++){
+                    (function(i){
+                      var name = data.entries[i].name;
+
+                      folder.append('<li class="item item-' + i + '">' + name + '</li>');
+                      $('.item-' + i).click(function(){
+                        console.log('ok ' + i + ' ' + id);
+                      });
+                    })(i)
+                }
+              }
+            });
+          } else {
+            console.log('error ' + tag + name);
+          }
+        });
+      })(i)
+
+
     }
     folder.append('</ol>');
+    console.log(data.entries);
   }
   //error: (err) => console.log(err),
 });
