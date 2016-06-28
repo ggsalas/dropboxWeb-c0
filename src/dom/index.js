ffee6bd6675d9e112e855fff2d68ca9ca078f3a1
Gabriel Salas <ggsalas@gmail.com>
2016-06-28 12:57:57 -0300

    column styles... in progress

--- src/dom/index.js
+++ src/dom/index.js
@@ -1,5 +1,5 @@
 export default class Dom {
   orderListFrom({items, root, mapper} = {}) {
-    root.innerHTML = `<ol>${items.map(mapper).join('')}</ol>`
+    root.innerHTML = `<div class="container"><div class="row">${items.map(mapper).join('')}</div></div>`
   }
 }
