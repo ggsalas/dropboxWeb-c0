da6101b8f31cc58474366066fa677f809134372d
Gabriel Salas <ggsalas@gmail.com>
2016-08-05 17:29:10 -0300

    improove columns clear

--- src/dom/index.js
+++ src/dom/index.js
@@ -1,14 +1,13 @@
 export default class Dom {
-  orderListFrom({items, index, root, mapper, i} = {}) {
-    root.innerHTML += `${items.map(mapper).join('')}${index}`; 
+  orderListFrom({items, index, root, mapper} = {}) {
+    root.innerHTML += `${items.map(mapper).join('')}`; 
 
-    if (index % 2 == 0)
-      root.innerHTML += `<div class="clearfix visible-xs-block"></div>` 
-    if (index % 4 == 0)
-      root.innerHTML += `<div class="clearfix visible-sm-block"></div>`
-    if (index % 6 == 0){
-      root.innerHTML += `<div class="clearfix visible-md-block"></div>`
-      root.innerHTML += `<div class="clearfix visible-lg-block"></div>`
-    } 
+    var xs, sm, md, lg;
+    (index % 2 == 0) ? (xs = "visible-xs-block ") : (xs = "");
+    (index % 4 == 0) ? (sm = "visible-sm-block ") : (sm = "");
+    (index % 6 == 0) ? (md = "visible-md-block ") : (md = "");
+    (index % 6 == 0) ? (lg = "visible-lg-block ") : (lg = "");
+
+    (index % 2 == 0) ? (root.innerHTML += `<div class="clearfix ${xs}${sm}${md}${lg}"></div>`) : ("");
   }
 }
