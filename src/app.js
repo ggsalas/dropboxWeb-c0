ab17d89f2b093120c22362915fe1ce6271111a90
Gabriel Salas <ggsalas@gmail.com>
2016-08-10 12:48:49 -0300

    fetch from google calendar, first step

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ require('imports?$=jquery!jquery')
 import 'bootstrap'
 import 'bootstrap/less/bootstrap.less'
 import './variables.less'
-// import ical from '../node_modules/ical'
 
 // Dropbox Browser
 const ACCESS_TOKEN = 'prnzgrtU38AAAAAAAAAAE-SiM917HjJloZISMGiouOPIkzaHhzShfUz92YhwGx8N'
@@ -16,16 +15,10 @@ browser.render({path: '/icb'})
 // Calendar Slider
 
 const CALENDAR_URL =  'https://calendar.google.com/calendar/ical/d8miajfcbt84e4on66bpcnj1r8%40group.calendar.google.com/public/basic.ics'
+const GET_URL = 'https://www.googleapis.com/calendar/v3/calendars/d8miajfcbt84e4on66bpcnj1r8%40group.calendar.google.com/events?maxResults=20&key=AIzaSyBDP5gGAWSQVHFRh7YDWpIWkCLzxOVPafU'
 const CALENDAR_NODE = document.getElementById('section-calendar')
 
-var ical = require('../node_modules/ical/ical')
-  , months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+fetch (GET_URL)
+.then(data => console.log(data))
 
-ical.fromURL(CALENDAR_URL, {}, function(err, data){
-  for (var k in data){
-    if (data.hasOwnProperty(k)){
-      var ev = data[k]
-      console.log("Conference", ev.summary, 'is in',  ev.location, 'on the', ev.start.getDate(), 'of', months[ev.start.getMonth()] );
-    }
-  }
-})
+
