ed7a8e8c9c9d63e867e5bc2b7429e70521293d08
Gabriel Salas <ggsalas@gmail.com>
2016-08-28 18:19:11 -0300

    calendar max events

--- src/calendar/calendar.js
+++ src/calendar/calendar.js
@@ -1,15 +1,16 @@
 export default class Calendar {
-  static get URL_TEMPLATE(){ return 'https://www.googleapis.com/calendar/v3/calendars/%ID%/events?%DATE%&key=AIzaSyAMj1Y8UDpwEmSFmTLm5XPXqpaW2KSyAkg';}
+  static get URL_TEMPLATE(){ return 'https://www.googleapis.com/calendar/v3/calendars/%ID%/events?maxResults=%MAX%&timeMin=%DATE%&key=AIzaSyAMj1Y8UDpwEmSFmTLm5XPXqpaW2KSyAkg';}
 
   constructor(calendar_id, view){
     this._calendar_id = calendar_id;
     this._view = view;
     let dateToday = new Date();
-    this._today = `timeMin=${dateToday.toJSON()}`;
+    this._today = dateToday.toJSON();
+    this.maxEvents = 10;
   }
   
   render(){
-    const url = Calendar.URL_TEMPLATE.replace('%ID%', this._calendar_id).replace('%DATE%', this._today)
+    const url = Calendar.URL_TEMPLATE.replace('%ID%', this._calendar_id).replace('%DATE%', this._today).replace('%MAX%', this.maxEvents)
 
     fetch (url) 
     .then(resp => resp.json())
