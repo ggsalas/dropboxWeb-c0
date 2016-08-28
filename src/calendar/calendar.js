4a539bb660ebe2ff6669bbd602cd21035bd7d5af
Gabriel Salas <ggsalas@gmail.com>
2016-08-28 18:11:42 -0300

    hide past events in calendar

--- src/calendar/calendar.js
+++ src/calendar/calendar.js
@@ -1,13 +1,16 @@
 export default class Calendar {
-  static get URL_TEMPLATE(){ return 'https://www.googleapis.com/calendar/v3/calendars/%ID%/events?key=AIzaSyAMj1Y8UDpwEmSFmTLm5XPXqpaW2KSyAkg';}
+  static get URL_TEMPLATE(){ return 'https://www.googleapis.com/calendar/v3/calendars/%ID%/events?%DATE%&key=AIzaSyAMj1Y8UDpwEmSFmTLm5XPXqpaW2KSyAkg';}
 
   constructor(calendar_id, view){
     this._calendar_id = calendar_id;
     this._view = view;
+    let dateToday = new Date();
+    this._today = `timeMin=${dateToday.toJSON()}`;
   }
   
   render(){
-    const url = Calendar.URL_TEMPLATE.replace('%ID%', this._calendar_id)
+    const url = Calendar.URL_TEMPLATE.replace('%ID%', this._calendar_id).replace('%DATE%', this._today)
+
     fetch (url) 
     .then(resp => resp.json())
     .then(data => {
