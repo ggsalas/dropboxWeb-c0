47f27bc56e90e85bed02ab4c53d654e0943ad83a
Gabriel Salas <ggsalas@gmail.com>
2016-09-04 22:31:35 -0300

    add config file for the app

--- src/app.js
+++ src/app.js
@@ -6,32 +6,29 @@ import './variables.less'
 import Calendar from './calendar/calendar'
 import View from './calendar/view'
 import Disqus from './disqus/disqus'
+import {config} from './config'
 
 /**
  * Dropbox Browser
  */
-// Paste access token of dropbox account and the folder (base path)
-const ACCESS_TOKEN = 'prnzgrtU38AAAAAAAAAAE-SiM917HjJloZISMGiouOPIkzaHhzShfUz92YhwGx8N'
-const BASE_PATH = '/icb';
-
+const ACCESS_TOKEN = config.dropboxAccessToken
+const BASE_PATH = config.dropboxFolder
 const ROOT_NODE = document.getElementById('root')
 const browser = new Browser({access_token: ACCESS_TOKEN, root: ROOT_NODE, basePath: BASE_PATH})
+
 browser.render({path: BASE_PATH})
 
 /**
  * Google Calendar Slider
  */
-// Paste calendar id
-const CALENDAR_ID =  'd8miajfcbt84e4on66bpcnj1r8@group.calendar.google.com'
+const CALENDAR_ID =  config.googleCalendarId
 const ROOT_ID = 'section-calendar-root'
 
-// comment this line to remove the calendar
-new Calendar(CALENDAR_ID, new View(ROOT_ID)).render();
+CALENDAR_ID == '' ? '' : new Calendar(CALENDAR_ID, new View(ROOT_ID)).render();
 
 /**
  * Disqus Chat
  */
 const ROOT_DISQUS = 'section-chat-root';
 
-// comment this line to remove the chat
-new Disqus(ROOT_DISQUS);
+config.displayDisqus == 'yes' ? new Disqus(ROOT_DISQUS) : '';
---
+++ src/config.js
@@ -0,0 +1,16 @@
+/**
+ * Configuration file for the site 
+ * Complete the options
+ */
+export const config = {
+  // DROPBOX
+  dropboxAccessToken : 'prnzgrtU38AAAAAAAAAAE-SiM917HjJloZISMGiouOPIkzaHhzShfUz92YhwGx8N',
+  dropboxFolder : '/icb',
+
+  // GOOGLE CALENDAR
+  googleCalendarId : 'd8miajfcbt84e4on66bpcnj1r8@group.calendar.google.com', // blank ('') for none
+
+  // DISQUS
+  displayDisqus : 'yes', // indicate yes or no
+}
+
